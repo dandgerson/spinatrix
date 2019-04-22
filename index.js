3ba2286734a52a4d686b09e3e0ec62258258b095
Dmitry G. Anderson <dandgerson@gmail.com>
2019-04-22 15:40:28 +0300

    refactor for passing all tests

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const x90 = (matrix) => {
   const rotated = [];
-  for (let i = matrix[0].length; i > 0; i -= 1) {
+  for (let i = 0; i < matrix[0].length; i += 1) {
     const row = [];
-    for (let j = 0; j < matrix.length; j += 1) {
+    for (let j = matrix.length - 1; j >= 0; j -= 1) {
       row.push(matrix[j][i]);
     }
     rotated.push(row);
   }
-  console.log(rotated);
   return rotated;
 };
 
-const x180 = null;
+const x180 = matrix => matrix
+  .map(row => row.slice().reverse())
+  .reverse();
 
 const x270 = (matrix) => {
   const rotated = [];
-  for (let i = matrix[0].length; i > 0; i -= 1) {
+  for (let i = matrix[0].length - 1; i >= 0; i -= 1) {
     const row = [];
     for (let j = 0; j < matrix.length; j += 1) {
       row.push(matrix[j][i]);
     }
     rotated.push(row);
   }
-  console.log(rotated);
   return rotated;
 };
 
